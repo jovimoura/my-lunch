536c0f80e071dde265bacc51ac4d898bcd4090cf
jovimoura <joaovictors.mouraa@gmail.com>
2024-02-07 14:55:30 -0300

    add items + product view + functionallities

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,64 @@
-import { View, FlatList } from 'react-native'
+import { View, FlatList, SectionList, Text } from 'react-native'
 import { CategoryButton } from '@/components/category-button'
 import { Header } from '@/components/header'
-import { CATEGORIES } from '@/utils/data/products'
-import { useState } from 'react'
+import { CATEGORIES, MENU } from '@/utils/data/products'
+import { useState, useRef } from 'react'
+import { Product } from '@/components/product'
+import { Link } from 'expo-router'
+import { useCartStore } from '@/stores/cart-store'
 
 export default function Home() {
+  const cartStore = useCartStore()
   const [category, setCategory] = useState(CATEGORIES[0])
+
+  const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0)
+
+  const sectionListRef = useRef<SectionList>(null)
 
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex(category => category === selectedCategory)
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0
+      })
+    }
   }
    return (
-    <View className='pt-10'>
-      <Header title='Faça seu pedido!' cartQuantityItems={1} />
+    <View className='flex-1 pt-10'>
+      <Header title='Faça seu pedido!' cartQuantityItems={cartQuantityItems} />
 
       <FlatList 
         data={CATEGORIES}
         keyExtractor={item => item}
         renderItem={({ item }) => <CategoryButton isSelected={item === category} title={item} onPress={() => handleCategorySelect(item)} />}
         horizontal
-        className='max-w-10 mt-5'
+        className='max-h-10 mt-5'
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator={false}
       />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />    
     </View>
   )
 }
---
+++ src/utils/functions/format-currency.ts
@@ -0,0 +1,6 @@
+export function formatCurrency(value: number): string {
+  return value.toLocaleString('pt-BR', {
+    style: "currency",
+    currency: 'BRL'
+  })
+}
